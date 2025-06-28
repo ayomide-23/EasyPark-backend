93b585678274066e4c858003e7271cbd8ec64a48
ayomide-23 <aderintoayomide204@gmail.com>
2025-06-28 21:53:27 +0100

    database connection update

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,13 @@
 <?php
-$host = "localhost";  
-$user = "root";            
-$pass = "";  
-$dbname = "pms"; 
+// $host = "localhost";  
+// $user = "root";            
+// $pass = "";  
+// $dbname = "pms"; 
+
+$host = "fdb1033.awardspace.net";
+$user = "4653705_easypark";
+$pass = "aappeter1";
+$dbname = "4653705_easypark";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
